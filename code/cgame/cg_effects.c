WofWca/quake3-better-gibs-mod

refactor: move `EV_GIB_PLAYER` handling to fn

code/cgame/cg_effects.c
@@ -1031,6 +1031,78 @@ void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 		}
 	} while (numGibs > 0);
 }
+void CG_GibPlayer2( const centity_t *cent, const entityState_t *es,
+					const clientInfo_t *ci ) {
+	int knockbackSpeed = cgs.g_gibsNewEvGibPlayerParmProtocol == 1
+		? es->eventParm * COMBAT_EV_GIB_PLAYER_ARG_DIVISOR
+		// Just use the default knockback speed for 100 damage.
+		: 100 * 1000 / COMBAT_PLAYER_MASS;
+
+	// Apparently at this point `es->pos.trDelta` doesn't yet have
+	// the knockback from the damage that gibbed us,
+	// so we have to differentiate between self and non-self
+	// during regular (non-demo non-spectator) gameplay.
+	const qboolean usePredictedPs =
+		es->number == cg.snap->ps.clientNum &&
+		!cg.demoPlayback &&
+		!(cg.snap->ps.pm_flags & PMF_FOLLOW);
+	const vec3_t *vel = usePredictedPs
+		? (const vec3_t*)&cg.predictedPlayerState.velocity
+		: &es->pos.trDelta;
+
+	lerpFrame_t torsoAnimation = es->number == cg.snap->ps.clientNum
+		// `cent->pe.torso` appears to be not good for self,
+		// unlike `cg.predictedPlayerEntity`,
+		// even during `demoPlayback` and `PMF_FOLLOW`,
+		// so we're not using `usePredictedPs` here.
+		? cg.predictedPlayerEntity.pe.torso
+		: cent->pe.torso;
+	vec3_t torsoAngles;
+
+	// TODO fix: things like `origin` and `angles`
+	// are not in complete sync between clients,
+	// so this seed is not always the same for all players.
+	int randSeed = es->number;
+	randSeed = Q_rand(&randSeed) + es->clientNum;
+	randSeed = Q_rand(&randSeed) + es->eventParm;
+	randSeed = Q_rand(&randSeed) + cgs.levelStartTime;
+	// TODO fix: this varies from client to client.
+	// So for now we round it to make it in sync ~95% of the time.
+	randSeed = Q_rand(&randSeed) + cg.snap->serverTime / 2048;
+	if ( ci ) {
+		randSeed = Q_rand(&randSeed) + ci->name[0];
+	}
+
+	// Torso animation angles seem to be in better sync
+	// between the local state and how others see us,
+	// and overall are closer to other player's viewangles
+	// than `cent->lerpAngles`.
+	// `cent->lerpAngles`, seems to sometimes be pointing
+	// in a completely different direction than the player's body
+	// at the time of death.
+	// Moreover, for non-self pitch seems to be always
+	// not very far from 0.
+	// This could be related to `LookAtKiller()`.
+	// Also see `CG_PlayerAngles`.
+	torsoAngles[PITCH] = torsoAnimation.pitchAngle;
+	torsoAngles[YAW] = torsoAnimation.yawAngle;
+	torsoAngles[ROLL] = 0;
+
+	if ( cg_debugGibs.integer & 0x04 ) {
+		CG_Printf("EV_GIB_PLAYER:");
+		CG_Printf(" time "S_COLOR_GREEN"%i",
+			cg.time );
+		CG_Printf(", clientNum "S_COLOR_GREEN"%i",
+			es->clientNum );
+		CG_Printf(", usePredictedPlayerState %s%i\n",
+			usePredictedPs ? S_COLOR_GREEN : S_COLOR_CYAN,
+			usePredictedPs );
+	}
+
+	CG_GibPlayer( cent->lerpOrigin, torsoAngles, *vel, knockbackSpeed,
+		&torsoAnimation, randSeed );
+}
+
 void CG_GibPlayerOld( const vec3_t playerOrigin ) {
 	vec3_t	origin, angles, velocity;
 

WofWca/baseq3a

code/cgame/cg_effects.c
@@ -1044,6 +1044,78 @@ void CG_GibPlayer( const vec3_t playerOrigin, const vec3_t playerAngles,
 		}
 	} while (numGibs > 0);
 }
+void CG_GibPlayer2( const centity_t *cent, const entityState_t *es,
+					const clientInfo_t *ci ) {
+	int knockbackSpeed = cgs.g_gibsNewEvGibPlayerParmProtocol == 1
+		? es->eventParm * COMBAT_EV_GIB_PLAYER_ARG_DIVISOR
+		// Just use the default knockback speed for 100 damage.
+		: 100 * 1000 / COMBAT_PLAYER_MASS;
+
+	// Apparently at this point `es->pos.trDelta` doesn't yet have
+	// the knockback from the damage that gibbed us,
+	// so we have to differentiate between self and non-self
+	// during regular (non-demo non-spectator) gameplay.
+	const qboolean usePredictedPs =
+		es->number == cg.snap->ps.clientNum &&
+		!cg.demoPlayback &&
+		!(cg.snap->ps.pm_flags & PMF_FOLLOW);
+	const vec3_t *vel = usePredictedPs
+		? (const vec3_t*)&cg.predictedPlayerState.velocity
+		: &es->pos.trDelta;
+
+	lerpFrame_t torsoAnimation = es->number == cg.snap->ps.clientNum
+		// `cent->pe.torso` appears to be not good for self,
+		// unlike `cg.predictedPlayerEntity`,
+		// even during `demoPlayback` and `PMF_FOLLOW`,
+		// so we're not using `usePredictedPs` here.
+		? cg.predictedPlayerEntity.pe.torso
+		: cent->pe.torso;
+	vec3_t torsoAngles;
+
+	// TODO fix: things like `origin` and `angles`
+	// are not in complete sync between clients,
+	// so this seed is not always the same for all players.
+	int randSeed = es->number;
+	randSeed = Q_rand(&randSeed) + es->clientNum;
+	randSeed = Q_rand(&randSeed) + es->eventParm;
+	randSeed = Q_rand(&randSeed) + cgs.levelStartTime;
+	// TODO fix: this varies from client to client.
+	// So for now we round it to make it in sync ~95% of the time.
+	randSeed = Q_rand(&randSeed) + cg.snap->serverTime / 2048;
+	if ( ci ) {
+		randSeed = Q_rand(&randSeed) + ci->name[0];
+	}
+
+	// Torso animation angles seem to be in better sync
+	// between the local state and how others see us,
+	// and overall are closer to other player's viewangles
+	// than `cent->lerpAngles`.
+	// `cent->lerpAngles`, seems to sometimes be pointing
+	// in a completely different direction than the player's body
+	// at the time of death.
+	// Moreover, for non-self pitch seems to be always
+	// not very far from 0.
+	// This could be related to `LookAtKiller()`.
+	// Also see `CG_PlayerAngles`.
+	torsoAngles[PITCH] = torsoAnimation.pitchAngle;
+	torsoAngles[YAW] = torsoAnimation.yawAngle;
+	torsoAngles[ROLL] = 0;
+
+	if ( cg_debugGibs.integer & 0x04 ) {
+		CG_Printf("EV_GIB_PLAYER:");
+		CG_Printf(" time "S_COLOR_GREEN"%i.%03is",
+			cg.time / 1000, cg.time % 1000 );
+		CG_Printf(", targ "S_COLOR_GREEN"%i",
+			es->number );
+		CG_Printf(", usePredictedPs %s%i\n",
+			usePredictedPs ? S_COLOR_GREEN : S_COLOR_CYAN,
+			usePredictedPs );
+	}
+
+	CG_GibPlayer( cent->lerpOrigin, torsoAngles, *vel, knockbackSpeed,
+		&torsoAnimation, randSeed );
+}
+
 void CG_GibPlayerOld( const vec3_t playerOrigin ) {
 	vec3_t	origin, angles, velocity;
 