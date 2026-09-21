fe1380c0f68dc21871da8f5ee6c170a898aa595e
WofWca <wofwca@protonmail.com>
2026-09-21 19:58:27 +0400

    fix: `cg_drawCrosshairNames` appearing wrongly
    Fixes https://github.com/ioquake/ioq3/issues/732.
    That is quite a significant bug IMO,
    sometimes akin to wall-hacking,
    i.e. sometimes you get to know when a player just appeared in your PVS
    (potentially visible set), i.e. is just behind a wall.
    
    This should also fix other similar manifestations of this bug
    because there are quite a lot of `CG_Trace` usages.
    E.g. the lightning bolt trace seems to be affected.
    But I think the crosshair names gonna be
    the most prominent manifestation.
    
    I have tested this and verified that collision detection
    seems to still work fine, and that crosshair names still appear
    like they should.

--- code/cgame/cg_predict.c
+++ code/cgame/cg_predict.c
@@ -42,6 +42,35 @@ void CG_BuildSolidList( void ) {
 		cent = &cg_entities[ snap->entities[ i ].number ];
 		ent = &cent->currentState;
 
+		// All consumers of `cg_solidEntities`
+		// (i.e. `CG_ClipMoveToEntities` and `CG_PointContents`)
+		// read the entities' `currentState` or `lerpOrigin`
+		// (which is (generally) a lerp between `currentState` and `nextState`).
+		// So we must ensure that `cg_solidEntities`
+		// only contains entities present in `cg.snap` (and not `cg.nextSnap`).
+		// Otheriwse we get bugs like
+		// https://github.com/ioquake/ioq3/issues/732,
+		// i.e. `CG_ScanForCrosshairEntity` using a player position
+		// from a very old snap.
+		//
+		// We still do check `nextState.solid` (below) though,
+		// in order to drop an entity from the solid list
+		// as soon as it stops being solid in the next snap
+		// (e.g. player getting gibbed).
+		//
+		// Thoughts:
+		// Maybe there was a bigger idea behind trying to use `cg.nextSnap`
+		// for building the solid list.
+		// Looking at the fact that we're in a file named `cg_predict.c`,
+		// maybe it was better movement prediction,
+		// i.e. maybe detecting a collision as soon as a solid
+		// is in `cg.nextSnap` but not yet in `cg.snap`.
+		// But until we're sure that we don't access invalid state,
+		// let's just have this check.
+		if ( !cent->currentValid ) {
+			continue;
+		}
+
 		if ( ent->eType == ET_ITEM || ent->eType == ET_PUSH_TRIGGER || ent->eType == ET_TELEPORT_TRIGGER ) {
 			cg_triggerEntities[cg_numTriggerEntities] = cent;
 			cg_numTriggerEntities++;
